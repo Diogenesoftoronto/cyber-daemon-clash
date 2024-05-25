34bbaf8420206bd22eb318a737def218c55f0ab6
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2024-05-24 21:50:19 -0400

    Create send-message and process-message
    Send and process message now compile and are created. They are
    primitive functions that allow daemons to communicate. They still do
    not really work though. Some further formatting and corrections to
    other functions and dependencies were also made.

--- main.lisp
+++ main.lisp
@@ -28,11 +28,13 @@
                  :defaults (config-directory)))
 
 (defun load-daemons (&key (path (daemon-path)) reset)
-  (ensure-directories-exist path) (cond ((or reset
+  (ensure-directories-exist path)
+  (cond ((or reset
              (not (probe-file path)) (< (file-write-date path)
                 (file-write-date (merge-pathnames "daemons.lisp"
                 (data-root)))))
-         (load-mapping (merge-pathnames "daemons.lisp" (data-root))) (when
+         (load-mapping (merge-pathnames "daemons.lisp" (data-root)))
+	 (when
          (and (probe-file path) (null reset))
            (load-mapping path))
          (save-daemons :path path))
@@ -42,12 +44,10 @@
 (defun save-daemons (&key (path (daemon-path)))
   (ensure-directories-exist path) (save-mapping path))
 
-;; Daemon list contains all the daemons. I
-;; was going to use this as a sb-ext:defglobal but this is not known at comptime.
-(setf +daemon-list+ (load-daemons)) 
+
 
 (define-event daemon-died (event))
- 
+(setf +daemons-list+ (list))
 ;; I need to make sure that there is
 ;; actually daemon-sprite.json file in the data/.  
 (define-asset (daemonbench sprite) sprite-data #p"daemon-sprite.png")
@@ -66,35 +66,108 @@
 ;; This really will need to be a macro i think because i need to
 ;; specify a particular daemon in the daemon pool in order to construct them
 ;; dynamically... I think maybe i could use a struct and constructor for them?
-(define-shader-entity a-daemon (animated-sprite transformed-entity
+(define-shader-entity daemon (animated-sprite transformed-entity
   collision-body listener)
-  (:default-initargs :sprite-data (asset 'daemonbench 'a-daemon))) 
+  (daemon-list :default-initarg :daemon-list (list)))
 ;; daemons should have some initialization standards, for example sometype
 ;; of indeterminate location, and resource contraints.
 ;; Definining constriants will be difficult. The daemon should also have
 ;; a set of base behaviours that are available to take.  
 
 ;; Test daemon inherits methods of a-daemon
-(define-shader-entity test-daemon (vertex-entity a-daemon)
-  (vertex-array :initform (// 'trial 'unit-cube))) 
+(define-shader-entity test-daemon (vertex-entity daemon)
+  
+  (:default-initargs :sprite-data (asset 'daemonbench 'daemon))) 
+
+(setf +broadcasted-daemons+ '()) ;; broadcasted daemons are ones that are visible to all
+;; daemons, when you ping you are added to the broadcasted daemons.
+
+;; 
+(defun extend-list-with-entry (obj place accessor)
+  (let
+      ((accessed (getf place accessor)))
+    (push obj accessed)))
+
+;; -> returns bool for it being nil or not, really considering creating the pbool type for real booleans
+(defun nil? (q &optional p &rest args)
+  (if (not q)
+      (if (not p)
+          (loop for (r s) on args by #'cddr
+                always (nil? r s))
+	 T)
+    NIL))
+
+(defun not-nil? (q &optional p &rest args) (not (nil? q p args)))
+
+;; This function must do resource constraint checking, if resource are out then this should immediately stop.
+(defun send-message (sender sendee message &rest messages &key accessor &allow-other-keys)
+  (cond
+    ((nil? message (car messages) (cdr messages)) ;; both NIL?
+     (loop for m in messages do (send-message sender sendee m)))  
+    ((not-nil? message (car messages) (cdr messages))
+     (send-message sender sendee message)
+     (loop for m in message do (send-message sender sendee m))) ;; [cond form 1]
+    ((equal message 'ping) ;; condition
+     (send-message sendee sender 'pong) ;; action 'clauses'...
+     (extend-list-with-entry sendee sender accessor))
+    ((equal message 'pong)
+     (extend-list-with-entry sendee sender accessor))
+    ((not message)
+     (send-message sender sendee message))))
+
+;; In addition, to adding messages to the daemon-list of the accessor, process-message should also do some work with the messages
+;; i.e. process-message should PARSE the messages and then do actions or behaviours based on them, this may be possible
+;; via a passed in parse function. NOTE: One thing to remember is that send-message and process-message should be given their
+;; accessors otherwise they cannot extend their lists with the extender, WARNING: current implementation will result in infinite recursion.
+;; This function must do resource constraint checking, if resource are out then this should immediately stop.
+(defun process-message (reciever recievee message &rest messages &key accessor parser &allow-other-keys)
+  (cond
+    ((nil? message (car messages) (cdr messages)) ;; both NIL?
+     (loop for m in messages do (process-message reciever recievee m)))  
+    ((not-nil? message (car messages) (cdr messages))
+     (process-message reciever recievee message)
+     (loop for m in message do (process-message reciever recievee m))) ;; [cond form 1]
+    ((equal message 'ping) ;; condition
+     (process-message recievee reciever 'pong) ;; action 'clauses'...
+     (extend-list-with-entry recievee reciever accessor))
+    ((equal message 'pong)
+     (extend-list-with-entry recievee reciever accessor))
+    ((not message)
+     (process-message reciever recievee message))))
+
+(define-handler (setf daemon-list)
+    ()) ;; filter from the global daemons, a list of daemons that have been pinged/sent orrecieved messages to/from this daemon.
 
 ;; Not sure what i should really be putting here will have to figure
 ;; it out. This looks for other daemons.
-(define-handler (a-daemon ping) (tt)
-  ()) 
+(define-handler (daemon ping) (other-daemon)
+  (send-message 'daemon 'other-daemon)) 
 
-(define-handler (a-daemon spawn)
-    (push (make-instance 'a-daemon) +daemon-list+))
+(define-handler (daemon spawn) ()
+  (push (make-instance (class-of daemon)) +daemon-list+))
+
+(define-handler (daemon sleep) (tt)
+  ()) ;; will do nothing until 
 ;; I need to find a way to add them as part of the scene as they spawn.
 ;; I wonder if adding them to the daemon list is what i should do?
 ;; Also I want this to return not just an instance of a-daemon,
 ;; but I want it to be like 'this' when it is called on test-daemon
 ;; for example, I want it to spawn a test daemon and add it to the daemon list.
    
-(define-handler (a-daemon tick) (tt)
+(define-handler (daemon tick) (tt)
   ;; vx horizontal, vy vertical axis spinning.  
-  (setf (orientation a-daemon) (qfrom-angle +vx+ tt)))
+  (setf (orientation daemon) (qfrom-angle +vx+ tt)))
 
+;; Daemon list contains all the daemons. I
+;; was going to use this as a sb-ext:defglobal but this is not known at comptime.
+
+;; (let (bindings*) body)
+;; (let ((ds (list))) body)
+;; (let ((ds (list))) (loop ...) ds)
+
+(setf +daemon-list+ (let ((ds (list))) (loop repeat 5
+					    do (push 'test-daemon ds)))
+			  ds)
 
 ;; shinmera does stuff with this, not sure will check later.  
 (define-event character-died (event)) 
@@ -124,8 +197,8 @@
 	;; The idea is that this should play the animation for each of the daemons.
 	;; consider dolist as this makes more sense than, `for` because of the lack
 	;; of syntax.
-  (loop for dd in +daemon-list+
-	(enter (make-instance 'dd)) scene)
+  (loop for dd in +daemon-list+ do
+	(enter (make-instance 'dd) scene))
     (enter (make-instance 'my-character) scene) 
     (enter (make-instance '2d-camera) scene)
   	 ;; Consider using a custom camera that will zoom into particular
